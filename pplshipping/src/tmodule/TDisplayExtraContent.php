c2566779567e2d9ef37e252dbfa56ca98b6da44a
jpokoraelx <jpokora@elinkx.cz>
2025-07-11 13:05:42 +0200

    Oprava instalace

--- pplshipping/src/tmodule/TDisplayExtraContent.php
+++ pplshipping/src/tmodule/TDisplayExtraContent.php
@@ -12,7 +12,7 @@ trait TDisplayExtraContent {
 
     public function hookDisplayHeader($params)
     {
-        $assetGeneratedName = "-1.0.1";
+        $assetGeneratedName = "-1.0.2";
         /**
          * @var \pplshipping $this
          */
